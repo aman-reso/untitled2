339ccf11b400267f86185b1e95221ee5fba2167d
aman-reso <amansecxcosx@gmail.com>
2023-02-01 16:49:02 +0530

    entry point created

--- lib/SampleCode.dart
+++ lib/SampleCode.dart
@@ -1,13 +1,81 @@
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:trainman_booking_sdk/trainman_booking_sdk.dart';
+import 'package:trainman_booking_sdk/src/source/models/pdfDataModel.dart';
 
-// class SampleCode extends StatelessWidget {
-//   const SampleCode({Key? key}) : super(key: key);
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     //trainman booking sdk
-//     return const InitiateTmBookingSdk();
-//   }
-// }
+class EntryPage extends StatelessWidget {
+  const EntryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(title: const Text("Traiman Booking sdk"),),
+      body: Column(
+        children: [
+          const Text('Demo of Trainman Booking sdk'), // ← Change to this.
+          ElevatedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const LoginSection()),
+              );
+            },
+            child: const Text('Open Booking sdk'),
+          ),
+        ],      // ← 7
+      ),
+    );
+  }
+}
+
+
+class LoginSection extends StatelessWidget implements TmBookingInterface {
+  const LoginSection({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    context = context;
+
+    return InitiateTmBookingSdk(tmBookingInterface: this,authToken: "aman");
+  }
+
+  @override
+  void backPressed(BuildContext context) {
+
+  }
+
+  @override
+  void downloadTicketWithUrl(PdfDataModel? pdfDataModel, String source) {
+
+  }
+
+  @override
+  void openCallSupport(String? mobileNum, String source) {
+
+  }
+
+  @override
+  void openEmailSupport(String? emailId, String source) {
+
+  }
+
+  @override
+  void openLoginPage(String message, String source) {
+
+  }
+
+  @override
+  void openUrlExternally(String? url, String source) {
+
+  }
+
+  @override
+  void refreshToken(String? pastAuthToken, String source) {
+
+  }
+
+
+
+
+}
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,10 @@
-import 'dart:developer';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:trainman_booking_sdk/src/source/models/pdfDataModel.dart';
 import 'package:trainman_booking_sdk/trainman_booking_sdk.dart';
+import 'package:untitled2/SampleCode.dart';
 
 void main() {
-  runApp(const LoginSection());
+  runApp(const MaterialApp(home: EntryPage()));
 }
 
-class LoginSection extends StatelessWidget implements TmBookingInterface {
-  const LoginSection({Key? key}) : super(key: key);
-  static const platform = MethodChannel('tmChannel');
-  @override
-  Widget build(BuildContext context) {
-    context = context;
-
-    return InitiateTmBookingSdk(tmBookingInterface: this,authToken: "aman");
-  }
-
-  Future<void> handleMethod(MethodCall call) async {
-    switch (call.method) {
-      case "login":
-        print(call.arguments);
-        break;
-      default:
-        throw PlatformException(
-          code: 'Unimplemented',
-          details: "The app doesn't implement ${call.method} method",
-        );
-    }
-  }
-
-  @override
-  void backPressed(BuildContext context) {
-    Navigator.pop(context);
-  }
-
-  @override
-  void openLoginPage(String message, String source) {
-    InitiateTmBookingSdk.updateAccessToken("aman", "kumar");
-  }
-
-  @override
-  void refreshToken(String? pastAuthToken, String source) {
-    log(source);
-  }
-
-  @override
-  void openCallSupport(String? mobileNum, String source) {
-    // TODO: implement openCallSupport
-  }
-
-  @override
-  void openEmailSupport(String? emailId, String source) {
-
-  }
-
-  @override
-  void downloadTicketWithUrl(String? url, String source) {
-    // TODO: implement downloadTicketWithUrl
-  }
-
-}
